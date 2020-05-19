204431a90d700061613909e5cf3ee3bf708c52d1
saurav278 <52128941+saurav278@users.noreply.github.com>
2020-05-19 16:57:09 +0530

    Add files via upload

---
+++ day1/program3.py
@@ -0,0 +1,12 @@
+x=int(input("Value of x : "))
+y=int(input("Value of y : "))
+  
+print ("Before swapping: ") 
+print("Value of x : ", x, " and y : ", y) 
+  
+# code to swap 'x' and 'y' 
+x, y = y, x 
+  
+print ("After swapping: ") 
+print("Value of x : ", x, " and y : ", y) 
+
