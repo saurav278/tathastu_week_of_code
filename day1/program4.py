33e5812cfb917592271980f16d491b83c387c466
saurav278 <52128941+saurav278@users.noreply.github.com>
2020-05-19 17:28:27 +0530

    Add files via upload

---
+++ day1/program4.py
@@ -0,0 +1,7 @@
+costPrice=float(input("enter the cost price "))
+sellingPrice=float(input("enter the selling price"))
+Profit=sellingPrice-costPrice
+print("Profit is ",Profit)
+new_sellingPrice=1.05*Profit + costPrice
+print("After adding 5% profit selling price is ",new_sellingPrice)
+
